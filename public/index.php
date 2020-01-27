eea7d5cbeffa1df7dd98946b7cdd58d7b63a19db
Pedro Antonio <pedroantonio.aragao@gmail.com>
2020-01-27 08:11:50 -0300

    Beta 1.0.1

---
+++ public/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
+    <link href="css/soon.css" rel="stylesheet">
+    <title>Expo</title>
+</head>
+<body>
+    <main class="principal">
+        <div class="conteudo-principal">
+            <div class="conteudo">
+                <img src="images/soon.png" alt="expo construir serra">
+            </div>
+            <div class="title">
+                <h1>
+                    Construção Civil - Imóveis - Serviços
+                </h1>
+                <h2>
+                    Estamos preparando uma exposição de produtos e serviços para construção civil e imóveis em geral
+                </h2>
+                <h3>
+                    EM BREVE NOSSO SITE ESTARÁ NO AR
+                </h3>
+            </div>
+            <div class="text">
+                <div class="text-one">
+                    <h4>
+                        Datas:
+                    </h4>
+                    <p>
+                        19 | 20 | 21
+                    </p>
+                    <p>
+                        JUNHO de 2020
+                    </p>
+                </div>
+                <div class="text-two">
+                    <h4>
+                        Contatos:
+                    </h4>
+                    <p>
+                        21 2743-4227
+                    </p>
+                    <p>
+                        21 99459-9368
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
